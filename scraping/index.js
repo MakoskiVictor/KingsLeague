3453f1340f93e3bd7489b474722bb8d0d80779a5
MakoskiVictor <makoski.ed@gmail.com>
2023-01-14 20:53:03 -0300

    added function team information

--- scraping/index.js
+++ scraping/index.js
@@ -1,6 +1,13 @@
 import * as cheerio from 'cheerio'
-import { writeFile } from 'node:fs/promises'
+import { writeFile, readFile } from 'node:fs/promises'
 import path from 'node:path'
+// Cuando usamos ESModules no podemos importar json directamente: import TEAM from '../db/team.json'
+// hay que usar el import assert type:
+// import TEAM from '../db/team.json' assert { tyoe: 'json' } => en este caso el inter no lo soporta, así que necesesitamos:
+const DB_PATH = path.join(process.cwd(), './db/')
+// Esto es un TOP LEVEL AWAIT
+const TEAM = await readFile(`${DB_PATH}/team.json`, 'utf-8').then(JSON.parse)
+// JSON.parse() => convierte lo que traemos a json
 
 const URLS = {
   leaderboard: 'https://kingsleague.pro/estadisticas/clasificacion/'
@@ -26,6 +33,9 @@ async function scrapingLiderboard () {
     yellowCards: { selector: '.fs-table-text_8', typeOf: 'number' },
     redCards: { selector: '.fs-table-text_9', typeOf: 'number' }
   }
+  // Para recuperar los la info de los teams.json
+  const getTeamIdFrom = ({ name }) => TEAM.find(team => team.name === name)
+
   // Limpieza de datos
   const cleanText = text => text
     .replace(/\t|\n|\s:/g, '')
@@ -46,7 +56,13 @@ async function scrapingLiderboard () {
 
       return [key, value]
     })
-    leaderboard.push((Object.fromEntries(leaderBoardEntries)))
+
+    const { team: teamName, ...leaderboardForTeam } = Object.fromEntries(leaderBoardEntries)
+    const team = getTeamIdFrom({ name: teamName })
+    leaderboard.push({
+      ...leaderboardForTeam,
+      team
+    })
   })
   return leaderboard
 }
@@ -54,5 +70,5 @@ async function scrapingLiderboard () {
 const leaderboard = await scrapingLiderboard()
 // Creo una ruta relativa de fichero
 // cwd => current working directory
-const filePath = path.join(process.cwd(), './db/leaderboard.json')
-await writeFile(filePath, JSON.stringify(leaderboard, null, 2), 'utf-8')
+// const filePath = path.join(process.cwd(), './db/leaderboard.json') => ya no lo necesitamos
+await writeFile(`${DB_PATH}/leaderboard.json`, JSON.stringify(leaderboard, null, 2), 'utf-8')
