7195a604709766939649755b5502427bd32375d6
MakoskiVictor <makoski.ed@gmail.com>
2023-01-16 23:06:23 -0300

    Added teams and presidents routes

--- api/index.js
+++ api/index.js
@@ -1,6 +1,8 @@
 import { Hono } from 'hono'
 import { serveStatic } from 'hono/serve-static.module'
 import leaderboard from '../db/leaderboard.json'
+import presidents from '../db/presidents.json'
+import team from '../db/Team.json'
 
 const app = new Hono()
 
@@ -9,12 +11,28 @@ app.get('/', (c) => {
     {
       endpoint: '/leaderboard',
       description: 'Returns the leaderboard'
+    },
+    {
+      endpoint: '/presidents',
+      descriptions: 'Returns the presidents info'
+    },
+    {
+      endpoint: '/teams',
+      descriptions: 'Returns the teams info'
     }
   ])
 })
 
 app.get('/leaderboard', (c) => {
   return c.json(leaderboard)
+})
+
+app.get('/presidents', (c) => {
+  return c.json(presidents)
+})
+
+app.get('/teams', (c) => {
+  return c.json(team)
 })
 
 app.get('/static/*', serveStatic({ root: './' }))
