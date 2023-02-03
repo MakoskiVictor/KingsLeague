ced38b05a106e9eb2204cc67f3ea58befdeb9a53
MakoskiVictor <makoski.ed@gmail.com>
2023-02-03 18:01:51 -0300

    utils added

---
+++ scraping/utils.js
@@ -0,0 +1,18 @@
+import * as cheerio from 'cheerio'
+
+export async function scrape (url) {
+  const res = await fetch(url)
+  const html = await res.text()
+  return cheerio.load(html)
+}
+
+export const URLS = {
+  leaderboard: 'https://kingsleague.pro/estadisticas/clasificacion/',
+  mpv: 'https://kingsleague.pro/estadisticas/mvp/'
+}
+
+// Limpieza de datos
+export const cleanText = text => text
+  .replace(/\t|\n|\s:/g, '')
+  .replace(/.*:/g, ' ')
+  .trim()
