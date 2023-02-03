61722a478e5f366d70e24df60ef90e127c67bd67
MakoskiVictor <makoski.ed@gmail.com>
2023-02-03 20:55:24 -0300

    restructuring utils

--- scraping/utils.js
+++ scraping/utils.js
@@ -1,4 +1,8 @@
 import * as cheerio from 'cheerio'
+import { writeDBFile } from '../db/index.js'
+import { scrapingLiderboard } from './leaderboard.js'
+import { logError, logInfo, logSuccess } from './log.js'
+import { getMvpList } from './mvp.js'
 
 export async function scrape (url) {
   const res = await fetch(url)
@@ -6,9 +10,15 @@ export async function scrape (url) {
   return cheerio.load(html)
 }
 
-export const URLS = {
-  leaderboard: 'https://kingsleague.pro/estadisticas/clasificacion/',
-  mvp: 'https://kingsleague.pro/estadisticas/mvp/'
+export const SCRAPINGS = {
+  leaderboard: {
+    url: 'https://kingsleague.pro/estadisticas/clasificacion/',
+    scraper: scrapingLiderboard
+  },
+  mvp: {
+    url: 'https://kingsleague.pro/estadisticas/mvp/',
+    scraper: getMvpList
+  }
 }
 
 // Limpieza de datos
@@ -16,3 +26,24 @@ export const cleanText = text => text
   .replace(/\t|\n|\s:/g, '')
   .replace(/.*:/g, ' ')
   .trim()
+
+export async function scrapeAndSave (name) {
+  const start = performance.now()
+  try {
+    const { scraper, url } = SCRAPINGS[name]
+    const $ = await scrape(url)
+
+    logInfo(`Scraping ${name}...`)
+    logSuccess(`${name} scaped successfully`)
+    const content = await scraper($)
+    logInfo(`Writting ${name} into DataBAse...`)
+    await writeDBFile(name, content)
+    logSuccess(`${name} written successfully`)
+  } catch (e) {
+    logError(e)
+  } finally {
+    const end = performance.now()
+    const time = (end - start) / 1000
+    logInfo(`[${name}] scraped in ${time} seconds`)
+  }
+}
