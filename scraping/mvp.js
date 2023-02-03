c04cfc6905a8b9edf6c4864a807d783b094c5447
MakoskiVictor <makoski.ed@gmail.com>
2023-02-03 18:40:12 -0300

    scraping mvp finished

--- scraping/mvp.js
+++ scraping/mvp.js
@@ -2,17 +2,16 @@
 import { writeDBFile, TEAM } from '../db/index.js'
 import { scrape, URLS, cleanText } from './utils.js'
 
+const MVP_SELECTORS = {
+  team: { selector: '.fs-table-text_3', typeOf: 'string' },
+  playerName: { selector: '.fs-table-text_4', typeOf: 'string' },
+  gamesPlayed: { selector: '.fs-table-text_5', typeOf: 'number' },
+  mvps: { selector: '.fs-table-text_6', typeOf: 'number' }
+}
+
 async function getMvpList () {
   const $ = await scrape(URLS.mvp)
   const $rows = $('table tbody tr')
-
-  const MVP_SELECTORS = {
-    rank: { selector: '.fs-table-text_1', typeof: 'number' },
-    team: { selector: '.fs-table-text_3', typeof: 'string' },
-    playerName: { selector: '.fs-table-text_4', typeof: 'string' },
-    gamesPlayed: { selector: '.fs-table-text_5', typeof: 'number' },
-    mvps: { selector: '.fs-table-text_6', typeof: 'number' }
-  }
 
   const getImageFromTeam = ({ name }) => {
     const { image } = TEAM.find(
@@ -20,4 +19,31 @@ async function getMvpList () {
     )
     return image
   }
+
+  const mvpSelectorEntries = Object.entries(MVP_SELECTORS)
+  const mvpList = []
+
+  $rows.each((index, el) => {
+    const mvpEntries = mvpSelectorEntries.map(([key, { selector, typeOf }]) => {
+      const rawValue = $(el).find(selector).text()
+      const cleanedValue = cleanText(rawValue)
+
+      const value = typeOf === 'number' ? Number(cleanedValue) : cleanedValue
+
+      return [key, value]
+    })
+    const { team: teamName, ...mvpData } = Object.fromEntries(mvpEntries)
+    const image = getImageFromTeam({ name: teamName })
+
+    mvpList.push({
+      ...mvpData,
+      rank: index + 1,
+      team: teamName,
+      image
+    })
+  })
+  return mvpList
 }
+
+const mvpList = await getMvpList()
+await writeDBFile('mvpList', mvpList)
