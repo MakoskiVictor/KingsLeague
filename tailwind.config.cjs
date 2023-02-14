3fb73344615cabcb8922f65617468bc258277248
MakoskiVictor <makoski.ed@gmail.com>
2023-02-14 18:36:48 -0300

    extend body and black

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  theme: {
+    extend: {
+      body: ['Archivo', 'system-ui', 'sans-serif'],
+      black: ['Archivo Black', 'system-ui', 'sans-serif']
+    }
+  },
+  plugins: []
 }
